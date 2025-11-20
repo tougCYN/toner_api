7ec19a6baeecce4ffa513b12e5dc2e90aaf9a069
tougCYN <mironzet99@gmail.com>
2025-11-20 11:07:26 +0100

    Delete toner_api — kopia directory

--- toner_api — kopia/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type");
-
-
-echo "Toner API działa. Użyj add_or_update.php, get_toner.php, list.php";
